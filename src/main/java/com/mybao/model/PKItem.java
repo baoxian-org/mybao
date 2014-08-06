31b3350cfc9be5124ad289625580ffc58f0049e3
xml777 <386002446@qq.com>
2014-08-06 16:15:25 +0800

    5

--- src/main/java/com/mybao/model/PKItem.java
+++ src/main/java/com/mybao/model/PKItem.java
@@ -6,5 +6,32 @@ package com.mybao.model;
  */
 public abstract class PKItem extends Item implements IPKItem
 {
+	private long id;
+	public long getId()
+	{
+		return id;
+	}
+	public void setId(long id)
+	{
+		this.id = id;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getNumber()
+	{
+		return number;
+	}
+	public void setNumber(String number)
+	{
+		this.number = number;
+	}
+	private String name;
+	private String number;
 	
 }
